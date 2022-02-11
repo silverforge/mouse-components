813ce31ad87fdf8dbfc0a05f0455a25838525c19
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2022-02-11 17:05:34 +0000

    loader light is done

--- src/components/moco-loader/moco-loader.tsx
+++ src/components/moco-loader/moco-loader.tsx
@@ -11,21 +11,21 @@ export class MocoLoader {
     return (
       <div class="container">
         <div class="container-inner">
-          <div class="dot01"></div>
-          <div class="dot02"></div>
-          <div class="dot03"></div>
-          <div class="dot04"></div>
-          <div class="dot05"></div>
-          <div class="dot06"></div>
-          <div class="dot07"></div>
-          <div class="dot08"></div>
-          <div class="dot09"></div>
-          <div class="dot10"></div>
-          <div class="dot11"></div>
-          <div class="dot12"></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
 
-        <div class="container-inner-inner">
+        <div class="container-inner-small">
           <div class="dot-small"></div>
         </div>
       </div>
---
+++ src/components/moco-loader/readme.md
@@ -0,0 +1,10 @@
+# moco-loader
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
